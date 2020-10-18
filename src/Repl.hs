a4f0e78280edf20c076fcd47cb343b425a6c523e
tkuriyama <taro.kuriyama@gmail.com>
2020-10-18 10:35:06 -0400

    Simple repl

--- src/Repl.hs
+++ src/Repl.hs
@@ -1,2 +1,22 @@
 module Repl where
 
+import Control.Monad (unless)
+import System.IO (stdout, hFlush)
+
+readInput :: IO String
+readInput = putStr "> "
+            >> hFlush stdout
+            >> getLine
+
+echo :: String -> IO ()
+echo = putStrLn 
+
+evalInput :: String -> String
+evalInput = id
+
+repl :: IO ()
+repl = do
+  input <- readInput
+  
+  unless (input == ":quit")
+       $ echo (evalInput input) >> repl
