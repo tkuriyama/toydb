9fe08ad1a4b8b9d65a6e62afafcca72428b9549d
tkuriyama <taro.kuriyama@gmail.com>
2020-10-20 22:53:54 -0400

    matching on expressions

--- src/Database.hs
+++ src/Database.hs
@@ -1,6 +1,31 @@
 module Database where
 
+import Syntax
+
 import Data.Text as T
 
-data Database = undefined
+data Database = TBD
 
+execExpr :: Database -> Expr -> Database
+execExpr db expr =
+  case expr of
+    (Create tbl cols) -> createTable db tbl cols
+    (Drop tbl) -> dropTable db tbl
+    (Select tbl conds) -> select db tbl conds
+    (Insert tbl fvs) -> insert db tbl fvs
+    (Delete tbl conds) -> delete db tbl conds
+
+createTable :: Database -> Table -> [Column] -> Database
+createTable = undefined
+
+dropTable :: Database -> Table -> Database
+dropTable = undefined      
+
+select :: Database -> Table -> [Condition] -> Database
+select = undefined
+
+insert :: Database -> Table -> [FieldValue] -> Database
+insert = undefined
+
+delete :: Database -> Table -> [Condition] -> Database
+delete = undefined      
