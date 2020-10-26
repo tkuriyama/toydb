1b7a8658333071f69cb1fef849616252d9461981
tkuriyama <taro.kuriyama@gmail.com>
2020-10-26 16:57:02 -0400

    commenting out incomplete Repl to unblock tests

--- src/Database/Repl.hs
+++ src/Database/Repl.hs
@@ -16,20 +16,22 @@ process db expr = do
   let parsedExpr = P.parseExpr expr
   case parsedExpr of
     Left err -> print err >> return Nothing
-    Right expr -> do
-      newdb <- execExpr db expr
+    Right expr' -> do
+      let newdb = execExpr db expr'
       return $ Just newdb
 
 repl :: IO ()
-repl = runInputT defaultSettings (loop Placeholder)
-  where
-    loop db = do
-      input' <- readInput
-      case input' of
-        --Nothing -> liftIO $ outputStrLn "Quitting..."
-        "quit" -> putStrLn "Quitting..."
-        input -> do
-          newDb <- liftIO $ process db input
-          case newDb of
-            Nothing -> loop db
-            Just newDb -> loop newDb
+repl = putStrLn ""
+
+-- repl = runInputT defaultSettings (loop Placeholder)
+--   where
+--     loop db = do
+--       input' <- readInput
+--       case input' of
+--         --Nothing -> liftIO $ outputStrLn "Quitting..."
+--         "quit" -> putStrLn "Quitting..."
+--         input -> do
+--           newDb <- liftIO $ process db input
+--           case newDb of
+--             Nothing -> loop db
+--             Just newDb -> loop newDb
